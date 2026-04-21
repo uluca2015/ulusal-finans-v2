73b75990f824cc82a67c56c7858bf061af092727
uluca2015 <uluca.s@hotmail.com>
2026-04-22 01:43:21 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 def root():
     return {"message": "Ulusal Finans V2 API çalışıyor 🚀"}
 
+@app.head("/")
+def root_head():
+    return {}
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
